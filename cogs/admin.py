d36cf6e2168b3d63708d69e261794117265dfb65
DartRuffian <skulldude327@gmail.com>
2021-10-23 21:19:03 -0500

    Admin Only commands
    Eval:
    - Runs a given code block and returns the output.
    
    Say:
    - Takes an optional list of channels to send a message to.
    - If no channels are specified, it is instead only sent the channel the command was run in.

---
+++ cogs/admin.py
@@ -0,0 +1,84 @@
+# Discord Imports
+import discord
+from discord.ext import commands
+from discord.ext.commands import Greedy
+
+# Eval Imports
+from contextlib import redirect_stdout
+import traceback
+import textwrap
+import io
+
+
+class Admin_Only(commands.Cog):
+    """ Admin Only commands, only meant to be used by the bot author """
+    def __init__(self, bot):
+        self.bot = bot
+        self._last_result = None
+    
+    def cleanup_code(self, content):
+        """ Removes code blocks from the code """
+        if content.startswith("```") and content.endswith("```"):
+            return "\n".join(content.split("\n")[1:-1])
+
+    @commands.command(hidden=True)
+    @commands.is_owner()
+    async def eval(self, ctx, *, body: str):
+        env = {
+            "bot": self.bot,
+            "ctx": ctx,
+            "channel": ctx.channel,
+            "author": ctx.author,
+            "guild": ctx.guild,
+            "message": ctx.message,
+            "_": self._last_result
+        }
+
+        env.update(globals())
+
+        body = self.cleanup_code(body)
+        stdout = io.StringIO()
+
+        to_compile = f"async def func():\n{textwrap.indent(body, '  ')}"
+
+        try:
+            exec(to_compile, env)
+        except Exception as e:
+            return await ctx.send(f"```py\n{e.__class__.__name__}: {e}\n```")
+
+        func = env["func"]
+        try:
+            with redirect_stdout(stdout):
+                ret = await func()
+        except Exception as e:
+            value = stdout.getvalue()
+            await ctx.send(f"```py\n{value}{traceback.format_exc()}\n```")
+        else:
+            value = stdout.getvalue()
+            try:
+                await ctx.message.add_reaction("\u2705")
+            except:
+                pass
+
+            if ret is None:
+                if value:
+                    await ctx.send(f"```py\n{value}\n```")
+            else:
+                self._last_result = ret
+                await ctx.send(f"```py\n{value}{ret}\n```")
+
+    @commands.command (
+        aliases=["s"],
+        hidden=True
+    )
+    @commands.is_owner()
+    async def say(self, ctx, channels: Greedy[discord.TextChannel]=None, *, message: str):
+        """ Basic say command, sends the message as a regular message """
+        await ctx.message.delete()
+        channels = channels or [ctx.channel]
+        for channel in channels:
+            await channel.send(message)
+
+
+def setup(bot):
+    bot.add_cog(Admin_Only(bot))
