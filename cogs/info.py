af43cb4802a12c309571fa107b98b2715a8b4bb3
DartRuffian <skulldude327@gmail.com>
2021-10-27 16:45:58 -0500

    Generic user info command
    user_info
    - author_info command but for any user.
    
    author_info
    - now uses a discord timestamp rather than just the user.created_at time

--- cogs/info.py
+++ cogs/info.py
@@ -1,5 +1,6 @@
 # Discord Imports
 import discord
+from discord.channel import DMChannel
 from discord.ext import commands
 
 # Uptime Imports
@@ -10,6 +11,9 @@ class Bot_Info(commands.Cog, name="Information"):
     """ Bot and Author Info """
     def __init__(self, bot):
         self.bot = bot
+    
+    def timestamp(self, time, mode="F"):
+        return f"<t:{str(time.timestamp()).split('.')[0]}:{mode}>"
 
     
     @commands.command (
@@ -30,7 +34,7 @@ class Bot_Info(commands.Cog, name="Information"):
     
     @commands.command (
         brief="Gives some basic info of the bot author.",
-        description="Gives some basic infomration of the bot's author: DartRuffian",
+        description="Gives some basic infomration of the bot's author: DartRuffian.",
         aliases=["author"]
     )
     async def author_info(self, ctx):
@@ -39,7 +43,7 @@ class Bot_Info(commands.Cog, name="Information"):
             "Name": {"text": author.name, "inline": True},
             "Discord Account": {"text": author, "inline": True},
             "Discord ID": {"text": f"`{author.id}`", "inline": True},
-            "Account Created At (UTC)": {"text": author.created_at, "inline": True},
+            "Account Created At": {"text": f"{self.timestamp(author.created_at)}\n{self.timestamp(author.created_at, 'R')}", "inline": True},
             "About Me": {"text": f"Hey there! First off I'd like to thank you for using my bot, {self.bot.user.name}. I'm an aspiring Software Engineer who specializes in primarily Python.", "inline": False},
             "Connected Accounts": {"text": f"[Github](https://github.com/{author.name}/ 'View {author.name} on Github')\n[Spotify](https://open.spotify.com/user/w8hah1vw86ysdslahmv5fsm9b/ 'View {author.name} on Spotify')\n[Steam](https://steamcommunity.com/id/{author.name}/ 'View {author.name} on Steam')", "inline": False}
         }
@@ -52,13 +56,57 @@ class Bot_Info(commands.Cog, name="Information"):
         await ctx.send(embed=embed)
     
     @commands.command (
-        brief="Returns the bot's prefix.",
-        description="Returns the bot's prefix for the current guild.",
+        brief="The generic version of `author_info`.",
+        description="The generic version of `author_info`, does not include connected accounts.",
+        aliases=["userinfo", "ui"]
+    )
+    @commands.guild_only()
+    async def user_info(self, ctx, user: discord.Member=None):
+        start_time = datetime.utcnow()
+        async with ctx.channel.typing():
+            user = user or ctx.author # Get the user's profile if no member is passed
+            status_dict = {
+                discord.Status.online: self.bot.cust_emojis["status_online"],
+                discord.Status.idle: self.bot.cust_emojis["status_idle"],
+                discord.Status.dnd: self.bot.cust_emojis["status_dnd"],
+                discord.Status.offline: self.bot.cust_emojis["status_offline"],
+            }
+            user_info = {
+                "Account Info": [
+                    f"{self.bot.cust_emojis['name_tag']} Username: {user}",
+                    f"{self.bot.cust_emojis['green_tick'] if user.nick is not None else self.bot.cust_emojis['gray_tick']} Nickname: {user.nick or 'No Nickname Set'}",
+                    f"{self.bot.cust_emojis['profile']} Profile Picture: [URL]({user.avatar_url} \"{user.name}'s Profile Picture\")",
+                    f"{self.bot.cust_emojis['authorized']} ID: {user.id}",
+                    f"{self.bot.cust_emojis['bot_tag']} Bot Account: {self.bot.cust_emojis['green_tick'] if user.bot else self.bot.cust_emojis['red_tick']}",
+                    f"{self.bot.cust_emojis['settings']} Created At: {self.timestamp(user.created_at)} - {self.timestamp(user.created_at, 'R')}",
+                ],
+                "Server Info": [
+                    f"{self.bot.cust_emojis['member_join']} Joined At: {self.timestamp(user.joined_at)} - {self.timestamp(user.joined_at, 'R')}",
+                    f"{self.bot.cust_emojis['owner']} Server Owner: {self.bot.cust_emojis['green_tick'] if user == ctx.guild.owner else self.bot.cust_emojis['red_tick']}",
+                    f"{self.bot.cust_emojis['booster']} Server Booster: {self.bot.cust_emojis['green_tick'] if user in ctx.guild.premium_subscribers else self.bot.cust_emojis['red_tick']}",
+                    f"{self.bot.cust_emojis['mention']} Top Role: {user.top_role.mention}"
+                ],
+                "Status Info": [
+                    f"🖥️ Desktop: {status_dict[user.desktop_status]}",
+                    f"🕸️ Web: {status_dict[user.web_status]}",
+                    f"📱 Mobile: {status_dict[user.mobile_status]}",
+                ]
+            }
+            user_embed = discord.Embed(color=self.bot.EMBED_COLOR, timestamp=datetime.utcnow())
+            user_embed.set_thumbnail(url=user.avatar_url)
+            for section_title, section_info in user_info.items():
+                user_embed.add_field(name=section_title, value="\n".join(section_info), inline=False)
+            user_embed.set_footer(text=f"Finished in: {round((datetime.utcnow() - start_time).total_seconds(), 2)} seconds.")
+            await ctx.send(embed=user_embed)
+    
+    @commands.command (
+        brief="Returns the bot's prefixes.",
+        description="Returns all available prefixes of the bot for the current guild.",
         aliases=["prefixes"]
     )
     async def prefix(self, ctx):
-        # if '<' not in prefix and '>' not in prefix else prefix for prefix in await self.bot.get_prefix(ctx.message) will place backticks at the begining and ending of
-        # entries from get_prefix() that do not contain both angle brackets.
+        # if '<' not in prefix and '>' not in prefix else prefix for prefix in await self.bot.get_prefix(ctx.message)
+        # will place backticks at the begining and ending of entries from get_prefix() that do not contain both angle brackets.
         # That way, only "normal" prefixes will be placed into single line code formatting, while mentions remain readable.
         embed = discord.Embed (
             title="__Prefix List:__",
--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from os import listdir, getcwd, environ # Load cogs/environment vars (token)
 # Define the bot
 intents = discord.Intents.default()
 intents.members = True
+intents.presences = True
 bot = commands.Bot (
     command_prefix=commands.when_mentioned_or("!"),
     owner_id=400337254989430784,
@@ -24,6 +25,26 @@ bot = commands.Bot (
 bot.BASE_DIR = getcwd()
 bot.EMBED_COLOR = 0x0E151D
 bot.launch_time = datetime.utcnow()
+bot.cust_emojis = {
+    "status_online"   : "<:status_online:902734042552737832>",
+    "status_idle"     : "<:status_idle:902734068532260864>",
+    "status_dnd"      : "<:status_dnd:902734083950534656>",
+    "status_offline"  : "<:status_offline:902734093970718770>",
+    "status_streaming": "<:status_streaming:902734109128949820>",
+    "green_tick"      : "<:green_tick:902733973162197034>",
+    "red_tick"        : "<:red_tick:902733992762155008>",
+    "gray_tick"       : "<:gray_tick:902734001859620886>",
+    "member_join"     : "<:member_join:902734371814010940>",
+    "bot_tag"         : "<:bot_tag:902734116271849512>",
+    "github"          : "<:github:902733789007061072>",
+    "mention"         : "<:mention:902737646135160862>",
+    "name_tag"        : "<:name_tag:902737654066597898>",
+    "settings"        : "<:settings:902737667823919114>",
+    "authorized"      : "<:authorized:902737626606501901>",
+    "profile"         : "<:profile:902756105074135052>",
+    "owner"           : "<:owner:902757689409568840>",
+    "booster"         : "<:booster:902757697164816394>",
+}
 
 
 # Events
