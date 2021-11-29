6f0b8f895678f76fad668fdf1263d764b77cc206
DartRuffian <skulldude327@gmail.com>
2021-11-28 23:05:48 -0600

    Rewrote the WikiTools commands to no longer use the WikpediaAPI

--- cogs/wiki.py
+++ cogs/wiki.py
@@ -3,16 +3,25 @@ import discord
 from discord.ext import commands
 
 # Other Imports
-import wikipediaapi  # https://pypi.org/project/Wikipedia-API/
-from utils import Utils
+from bs4 import BeautifulSoup
+import requests
 import os
+
+
+def cleanup_url(url: str = None) -> str:
+    if url is None:
+        return "https://en.wikipedia.org/wiki/Special:Random"
+
+    url = url.lower()
+    url = " ".join(elem.capitalize() for elem in url.split())
+    url = url.replace(" ", "_")
+    return f"https://en.wikipedia.org/wiki/{url}"
 
 
 class WikiTools(commands.Cog, name="Wikipedia Tools"):
     """A set of commands relating to Wikipedia"""
     def __init__(self, bot):
         self.bot = bot
-        self.wiki = wikipediaapi.Wikipedia("en")
 
     @commands.group(
         brief="Group of commands relating to Wikipedia articles.",
@@ -20,7 +29,7 @@ class WikiTools(commands.Cog, name="Wikipedia Tools"):
         aliases=["w"],
         invoke_without_command=True
     )
-    async def wiki(self, ctx, *args):
+    async def wiki(self, ctx):
         """Base command, serves only to group commands together"""
 
     @wiki.command(
@@ -28,36 +37,54 @@ class WikiTools(commands.Cog, name="Wikipedia Tools"):
         description="Takes a given search query and looks for a Wikipedia page with that name.",
         aliases=["s"]
     )
-    async def search(self, ctx, *, term: str):
-        term = term.lower().replace(" ", "_")
-        page = self.wiki.page(term)
+    async def search(self, ctx, *, url: str = None):
+        url = cleanup_url(url)
 
-        if not page.exists():
-            await ctx.send(f"There were no articles with the name of `{term}`, please try again.")
+        response = requests.get(url=url)
+        if response.status_code == 404:
+            await ctx.send(f"""Sorry, but it doesn't seem like {url} is a page that exists.
+Double check your spelling and try again, capitalization and spaces do not matter.""")
             return
 
-        await ctx.send(embed=self.bot.utils.embed_from_wiki_page(page))
+        soup = BeautifulSoup(response.content, "html.parser")
+        all_paragraphs = soup.find_all("p")
+        summary = [p.get_text() for p in all_paragraphs if p.get_text() != "\n"].pop()
+        image = soup.find_all("img")[0].get("src")
+
+        embed = discord.Embed(
+            description=summary,
+            color=self.bot.EMBED_COLOR
+        )
+        embed.set_image(url="https:" + image)
+        embed.set_author(name=f"{soup.find(id='firstHeading').string} (Click for Full Page)", url=url)
+        await ctx.send(embed=embed)
 
     @wiki.command(
         brief="Saves an entire Wikipedia article as a `.txt` file.",
         description="Takes a link to a Wikipedia page and saves it to a `.txt` file."
     )
     async def save(self, ctx, url: str):
-        term = url.replace("https://en.wikipedia.org/wiki/", "")
-        page = self.wiki.page(term)
+        url = url.replace("https://en.wikipedia.org/wiki/", "")
+        url = cleanup_url(url)
 
-        if not page.exists():
-            await ctx.send(f"There were no articles with the name of `{term}`, please try again.")
+        response = requests.get(url=url)
+        if response.status_code == 404:
+            await ctx.send(f"""Sorry, but it doesn't seem like {url} is a page that exists.
+Double check your spelling and try again, capitalization and spaces do not matter.""")
             return
+
+        soup = BeautifulSoup(response.content, "html.parser")
+        all_paragraphs = [text.get_text() for text in soup.find_all("p") if text.get_text() != "\n"]
+        title = soup.find(id='firstHeading').string.lower().replace(" ", "_")
 
         async with ctx.channel.typing():
             os.chdir(f"{self.bot.BASE_DIR}/resources")
             with open("latest_article.txt", "w", encoding="utf-8") as f:
-                f.write(page.text)
+                f.write("".join(all_paragraphs))
             with open("latest_article.txt", "rb") as f:
                 await ctx.send(
                     f"Saved all available text for the article at: <{url}>.",
-                    file=discord.File(f, f"{page.title}.txt")
+                    file=discord.File(f, f"{title}.txt")
                 )
             os.chdir(self.bot.BASE_DIR)
 
--- utils.py
+++ utils.py
@@ -2,7 +2,6 @@
 import discord
 
 # Other Imports
-from wikipediaapi import WikipediaPage
 import os
 
 
@@ -15,11 +14,3 @@ class Utils:
         with open("bot.log", "a") as f:
             f.write(message + "\n\n")
         os.chdir(self._bot.BASE_DIR)
-
-    def embed_from_wiki_page(self, page: WikipediaPage) -> discord.Embed:
-        embed = discord.Embed(
-            description=page.summary.split("\n")[0],
-            color=self._bot.EMBED_COLOR
-        )
-        embed.set_author(name=f"{page.title} (Click for Full Page)", url=page.fullurl)
-        return embed
