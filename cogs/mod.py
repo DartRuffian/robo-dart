06a5baf2fb109a159fa8f0fd77f0e728cd262694
DartRuffian <skulldude327@gmail.com>
2022-02-18 21:44:39 -0600

    Increased purge performance

--- cogs/mod.py
+++ cogs/mod.py
@@ -8,26 +8,6 @@ from discord.ext.commands import Greedy
 from datetime import datetime
 
 
-def check_message(message, tags) -> bool:
-    should_delete = True
-    # Check the message against the given arguments
-    if "ip" in tags and message.pinned:
-        should_delete = False
-    if "ib" in tags and message.author.bot:
-        should_delete = False
-    if "content" in str(tags):
-        text_to_clear = get_content_tag(tags)
-        if text_to_clear not in message.content.lower():
-            should_delete = False
-    return should_delete
-
-
-def get_content_tag(tags) -> str:
-    for tag in tags:
-        if tag.startswith("content"):
-            return tag[len("content "):].lower()  # get only the text
-
-
 def create_embed(ctx, message) -> discord.Embed:
     embed = discord.Embed(
             description=message,
@@ -111,57 +91,17 @@ class ModOnly(commands.Cog, name="Moderation"):
         aliases=["delete", "del", "clear", "cleanup"]
     )
     @has_permissions(manage_messages=True)
-    async def purge(self, ctx, limit: Greedy[int], clear_until: Greedy[discord.Message], *, tags=None):
-        if tags is None:
-            tags = "--ip"
-
-        if limit == [] and clear_until == []:
-            # Neither is passed, use as a pseudo help command
-            args_desc = {
-                "--ip": "Ignore pinned messages. (Default)",
-                "--ib": "Ignore bot messages.",
-                "--content `<text>`": "Will only delete messages that contain the given text."
-            }
-            embed = discord.Embed(
-                description="Here's a list of all possible arguments and what they do.",
-                color=self.bot.EMBED_COLOR
-            )
-            for tag, meaning in args_desc.items():
-                embed.add_field(
-                    name=tag,
-                    value=meaning
-                )
-            await ctx.send(embed=embed)
-            return
-
+    async def purge(self, ctx, limit: Greedy[int], clear_until: Greedy[discord.Message] = None):
         await ctx.message.delete()
-        tags = [i.strip(" ") for i in tags.strip().split("--")]  # get arguments as a list
-        deleted_messages = {}
-        if limit:
-            async for message in ctx.channel.history(limit=limit[0]):
-                delete_message = check_message(message, tags)
 
-                if delete_message:
-                    count = deleted_messages.get(message.author, 0)
-                    count += 1
-                    deleted_messages.update({message.author: count})
-                    await message.delete()
+        if limit:
+            await ctx.channel.purge(limit=limit[0])
 
         elif clear_until:
             async for message in clear_until[0].channel.history(limit=100):
-                delete_message = check_message(message, tags)
-                if delete_message:
-                    count = deleted_messages.get(message.author, 0)
-                    count += 1
-                    deleted_messages.update({message.author: count})
-                    await message.delete()
-                    if message == clear_until[0]:
-                        break
-
-        deleted = sum(deleted_messages.values())
-        messages = [f"{deleted} message{' was' if deleted == 1 else 's were'} removed."]
-        messages.extend(f"- **{author}**: {count}" for author, count in deleted_messages.items())
-        await ctx.send("\n".join(messages), delete_after=10)
+                await message.delete()
+                if message == clear_until[0]:
+                    break
 
 
 def setup(bot):
